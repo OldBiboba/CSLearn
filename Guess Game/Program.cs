a5b4da1e9c45ec436d73c0af7393a3844ae225be
Pavel <example@example.com>
2025-08-15 20:40:17 +0300

    added guess-game

---
+++ Guess Game/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_Game {
+    internal class Program {
+        static void Main(string[] args) {
+            Random rand = new Random();
+            int num = rand.Next(1, 101);
+
+            Console.WriteLine("Угадай число от 1 до 100");
+
+            while (true) {
+                int userNum = 0;
+                try {
+                    userNum = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception) {
+                    Console.WriteLine("Некорректный ввод! Введите число от 1 до 100");
+                    continue;
+                }
+                
+                if (num > userNum) {
+                    Console.WriteLine($"Загаданное число больше {userNum}");
+                }
+                else if (num < userNum) {
+                    Console.WriteLine($"Загаданное число менше {userNum}");
+                }
+                else {
+                    Console.WriteLine("Вы угадали число!");
+                    Console.ReadLine();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Guess Game/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Guess Game")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Guess Game")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("640861bf-961a-4dd5-8a40-62e2ce188707")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
